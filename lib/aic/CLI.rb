64529cab526f830551515c621383c3962d0f669b
Katie Larson <katmae83@gmail.com>
2017-08-09 21:38:34 +0000

    creating UI methods in CLI

--- lib/aic/CLI.rb
+++ lib/aic/CLI.rb
@@ -1,24 +1,30 @@
 class Aic::CLI
 
   def call
-    puts "Come see what's at the Art Institute!"
-    puts "For current and upcoming exhibits, type 'exhibits'"
-    puts "For upcoming events, type 'events'"
-    puts "For general information, such as hours and admissions, type 'info'"
+    menu
     input = gets.strip
+    case input
+    when "exhibits"
+      exhibits
+    when "events"
+      events
+    when "info"
+      info
+    end
+  
     case input
     when "exhibits"
       puts "For current exhibits, enter 'current'"
       puts "For future exhibits, enter 'future'"
-      #new_input = gets.strip
-      #case new_input
-      #when "current"
-      #  puts "1. Gauguin: Artist as Alchemist"
-      #  puts "2. Saints and Heroes"
-      #when "future"
-      #  puts "1. Color Studies"
-      #  puts "2. Making Memories: Quilts as Souvenirs"
-      #end
+      new_input = gets.strip
+      case new_input
+      when "current"
+        puts "1. Gauguin: Artist as Alchemist"
+        puts "2. Saints and Heroes"
+      when "future"
+        puts "1. Color Studies"
+        puts "2. Making Memories: Quilts as Souvenirs"
+      end
     when "events"
       puts "To see a list of all events in the next year, type 'all'"
       puts "Enter a start and end date (MM/DD/YYYY) to see events during that time"
@@ -29,4 +35,16 @@ class Aic::CLI
       puts "Admission fees are based in part on state and city residency. Please select one of the following to see full admission costs (note that this does not include Fast Passes, Memberships, or combination tickets"
     end
   end
+end
+
+def menu
+  puts "Come see what's at the Art Institute!"
+  puts "For current and upcoming exhibits, type 'exhibits'"
+  puts "For upcoming events, type 'events'"
+  puts "For general information, such as hours and admissions, type 'info'"
+end
+
+def exhibits
+
+
 end
