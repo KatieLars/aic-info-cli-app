0f0e91176a141b3f4c953d17a33427c2096b3d32
Katie Larson <katmae83@gmail.com>
2017-08-09 21:14:09 +0000

    fleshing out menu

---
+++ lib/aic/CLI.rb
@@ -0,0 +1,9 @@
+class Aic::CLI
+
+  def call
+    puts "Come see what's at the Art Institute!"
+    puts "For current and upcoming exhibits, type 'exhibits'"
+    puts "For upcoming events, type 'events'"
+    puts "For general information, such as hours and admissions, type 'info'"
+  end
+end
