7ccb714e23d6675127a6bf67440d67672d85e520
Katie Larson <katmae83@gmail.com>
2017-08-09 21:27:21 +0000

    stubbing out UI

--- lib/aic/CLI.rb
+++ lib/aic/CLI.rb
@@ -5,5 +5,25 @@ class Aic::CLI
     puts "For current and upcoming exhibits, type 'exhibits'"
     puts "For upcoming events, type 'events'"
     puts "For general information, such as hours and admissions, type 'info'"
+    input = gets.strip
+    case input
+    when "exhibits"
+      puts "For current exhibits, enter 'current'"
+      puts "For future exhibits, enter 'future'"
+      #new_input = gets.strip
+      #case new_input
+      #when "current"
+      #  puts "1. Gauguin: Artist as Alchemist"
+      #  puts "2. Saints and Heroes"
+      #when "future"
+      #  puts "1. Color Studies"
+      #  puts "2. Making Memories: Quilts as Souvenirs"
+      #end
+    when "events"
+      puts "To see a list of all events in the next year, type 'all'"
+      puts "Enter a start and end date (MM/DD/YYYY) to see events during that time"
+      puts "To see certain types of events, enter one of the following"
+      puts "<list of types scraped from event page>"
+      puts
   end
 end
