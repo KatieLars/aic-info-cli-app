81b7bb31b40b453223562a3fe63c8b6ae0e70e51
Katie Larson <katmae83@gmail.com>
2017-08-09 21:15:14 +0000

    more files added

--- lib/aic.rb
+++ lib/aic.rb
@@ -1,5 +1,7 @@
-require "aic/info/cli/app/version"
-
+#this is your environment
+require "./lib/aic/version"
+require "./lib/aic/CLI"
+#list other specific files here that need to be required
 module Aic
 
         # Your code goes here...
