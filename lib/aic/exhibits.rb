c7df16d0c16cb0c1e7daaf550dbbfb354cfb3924
Katie Larson <katmae83@gmail.com>
2017-08-10 21:49:17 +0000

    final saves of 8/10

--- lib/aic/exhibits.rb
+++ lib/aic/exhibits.rb
@@ -1,6 +1,10 @@
 class Aic::Exhibits
-  attr_accessor :title, :exhibit_dates, :now_or_future, :url, :description, :location
+  attr_accessor :title, :date_range, :now_or_future, :url, :description, :location
+  @@current = []
+  @@future = []
 
+  #@@current & @@future is an array that stores all Exhibit objects that have an @now_or_future of "current" or "upcoming" (aka future)
+  #type lock @@current and @@future as Exhibit objects
   #Each Exhibit has: a title, a date, now or future category, a url, description, and location
     #this is a place to store data
     #@exhibit_dates should just return a string scraped from the Exhibits page
