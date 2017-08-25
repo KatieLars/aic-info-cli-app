04811b71a88b143fa0f19f828cad4795827fcf62
github username <github email address>
2017-08-25 16:34:53 -0500

    "working on outlier inputs #exhibit_info"

--- lib/aic/exhibit.rb
+++ lib/aic/exhibit.rb
@@ -24,23 +24,24 @@ class Aic::Exhibit
   def self.exhibit_info(type) #generates exhibit info depending on user input
     current_hash = {} #hash with index being key and exhibit tile as value
     scrape_from_web("http://www.artic.edu/exhibitions/#{type}")
-    send("#{type}").each.with_index(1) {|e, i| puts "#{i}. #{e.title}"}
     send("#{type}").each.with_index(1) {|e, i| current_hash[i] =  "#{e.title}"}
+    current_hash.each {|k,v| puts "#{k}. #{v}"}
     puts "Enter the name of the exhibit or its number for dates, times, and description"
     new_input = gets.strip
-    send("#{type}").detect do |exhibit|
-      if exhibit.title.include? (current_hash.detect {|k,v| v.include?("#{new_input}") || k == "#{new_input}".to_i}[1])
-       puts "#{exhibit.title}"
-       puts "#{exhibit.date_range}"
-       puts "#{exhibit.location}"
-       puts "#{exhibit.description}"
-       puts "#{exhibit.url}"
-       exit
-      #else
-      #  puts "Sorry! I didn't recognize that exhibit."
-      #  exhibit_info(type)
-      end #if statement end
-    end #each statement end
+    if current_hash.detect {|k,v| v.include?("#{new_input}") || k == "#{new_input}".to_i}
+      send("#{type}").each do |exhibit|
+        if exhibit.title.include? (current_hash.detect {|k,v| v.include?("#{new_input}") || k == "#{new_input}".to_i}[1])
+          puts "#{exhibit.title}"
+          puts "#{exhibit.date_range}"
+          puts "#{exhibit.location}"
+          puts "#{exhibit.description}"
+          puts "#{exhibit.url}"
+        end #inner if statement must stay
+      end #each end
+     elsif current_hash.none? {|k,v| v.include?("#{new_input}") || k == "#{new_input}".to_i}
+       puts "Sorry! I can't find that exhibit."
+       exhibit_info(type) #need to not accumulate lists . . . 
+     end #outer if statement
   end #exhibit_info
 
   def self.current #returns array of current Exhibit objects
