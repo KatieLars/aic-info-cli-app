d287a46034053d97465f6c69f1f320c75e5da708
github username <github email address>
2017-08-25 13:39:25 -0500

    "debugging #exhibit_info"

--- lib/aic/exhibit.rb
+++ lib/aic/exhibit.rb
@@ -24,25 +24,27 @@ class Aic::Exhibit
   def self.exhibit_info(type)
     #need to use the input of type to invoke new class methods
     current_hash = {} #hash with index being key and exhibit tile as value
-    self.scrape_from_web("http://www.artic.edu/exhibitions/#{type}")
-    self.send("#{type}").each.with_index(1) {|e, i| puts "#{i}. #{e.title}"}
-    self.send("#{type}").each.with_index(1) {|e, i| current_hash[i] =  "#{e.title}"}
+    scrape_from_web("http://www.artic.edu/exhibitions/#{type}")
+    send("#{type}").each.with_index(1) {|e, i| puts "#{i}. #{e.title}"}
+    send("#{type}").each.with_index(1) {|e, i| current_hash[i] =  "#{e.title}"}
     puts "Enter the name of the exhibit or its number for dates, times, and description"
     new_input = gets.strip
-    binding.pry
-    self.send("#{type}").each do |exhibit| #iterates over an array of Exhibit Objects and returns info for that event
+
+    send("#{type}").each do |exhibit| #iterates over an array of Exhibit Objects and returns info for that event
       if !new_input.to_i.is_a?(Integer) && exhibit.title == current_hash.each {|k,v| v.include?("#{new_input}")}[1]
        puts "#{exhibit.title}"
        puts "#{exhibit.date_range}"
        puts "#{exhibit.location}"
        puts "#{exhibit.description}"
        puts "#{exhibit.url}"
+       exit
      elsif new_input.to_i.is_a?(Integer) && exhibit.title == current_hash[new_input.to_i]
         puts "#{exhibit.title}"
         puts "#{exhibit.date_range}"
         puts "#{exhibit.location}"
         puts "#{exhibit.description}"
         puts "#{exhibit.url}"
+        exit
       else
         puts "Sorry! I didn't recognize that exhibit."
         self.exhibit_info("#{type}")
