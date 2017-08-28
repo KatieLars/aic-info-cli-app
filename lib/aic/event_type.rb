342a11d7bca0ad9933d5ee1e24c3415c5fe7a845
github username <github email address>
2017-08-28 13:52:10 -0500

    "defining #add_to_all and #add_events"

--- lib/aic/event_type.rb
+++ lib/aic/event_type.rb
@@ -1,9 +1,29 @@
 class Aic::EventType #HAS MANY Events
-  attr_accessor :name, :events
-  @@all = {}
+  attr_accessor :name, :events #events is a list of all events for a specific date
+  @@all = {} #key is event type, value is an array listing all events for that type
 
-  def initialize
+  def initialize(name)
     @events = []
+    @name = name
+  end
+
+  def self.add_to_all
+    new_event = Aic::Event.new
+    if @@all.none? {|k,v| k == new_event.type}
+      @@all[new_event.type] =
+    end
+
+  end
+
+  def add_events #adds Event object to @event array
+    new_event = Aic::Event.new
+    if new_event.type == @name
+      @events << new_event
+    end
+  end
+
+  def self.all
+    @@all
   end
 
   #EventTypes have: a list of all Event objects that correspond to them. They also have names generated from a scraper
