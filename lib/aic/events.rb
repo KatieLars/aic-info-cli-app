f64e6d6736d714929f27476d673172720cdba32c
github username <github email address>
2017-08-28 12:54:07 -0500

    "finishing scraper"

--- lib/aic/events.rb
+++ lib/aic/events.rb
@@ -1,6 +1,6 @@
 
 class Aic::Event # HAS ONE EventType, HAS ONE EventDate
-  attr_accessor :title, :type, :event_date, :time, :url, :summary
+  attr_accessor :title, :type, :event_date, :time, :url, :description
   @@all = []
 
   def self.scrape_from_web(url)
@@ -9,13 +9,12 @@ class Aic::Event # HAS ONE EventType, HAS ONE EventDate
     event_array.each do |xml_element| #try to refactor with send
       new_event = Aic::Event.new
       new_event.title = xml_element.css("div.col-wrapper.clearfix div.col-inner div.title.views-field.views-field-title").text
-      new_exhibit.type = xml_element.css("div.col-wrapper.clearfix div.col-inner div.views-field.views-field-taxonomy").text #should hook into EventType
-      new_exhibit.event_date = Chronic.parse(xml_element.css("div.col-wrapper.clearfix div.col-inner div.date.views-field").text)
-      new_exhibit.description = xml_element.css("div.views-field.views-field-body span.field-content").first.text
-      new_exhibit.url = "http://www.artic.edu" + xml_element.css("div.views-field.views-field-title span.field-content a").attribute("href").text
+      new_event.type = xml_element.css("div.col-wrapper.clearfix div.col-inner div.views-field.views-field-taxonomy").text #should hook into EventType
+      new_event.event_date = Chronic.parse(xml_element.css("div.col-wrapper.clearfix div.col-inner div.date.views-field").text)
+      new_event.description = xml_element.css("div.col-wrapper.clearfix div.col-inner div.summary.views-field p").text
+      new_event.url = "http://www.artic.edu" + xml_element.css("div.col-wrapper.clearfix div.col-inner div.title.views-field.views-field-title a").attribute("href").text
+      new_event.time = xml_element.css("div.col-wrapper.clearfix div.col-inner div.time.views-field").text
   end #scraper
-
-
   #event_date is Time object
   #Dates should only be added to the hash if there isn't already
   #type lock @time, @event_date
