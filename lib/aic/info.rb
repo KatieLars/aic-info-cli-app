de0d13fc785b4bf72ce1445d5340d0fed18f7b1f
github username <github email address>
2017-09-06 16:52:24 -0500

    "finishing touches to Free option of #prices"

--- lib/aic/info.rb
+++ lib/aic/info.rb
@@ -28,7 +28,6 @@ class Aic::Info
 
   def self.prices(residency)
     doc = Nokogiri::HTML(open("http://www.artic.edu/visit"))
-    binding.pry
 
     case residency
     when "General"
@@ -51,33 +50,28 @@ class Aic::Info
       free_desc = []
       free_hash ={} #key is header from free_info, value is description from free_desc
       doc.css("h2#FreeAdmissionOpportunities ~ h4").to_a.each do |e|
-        free_info << e.text
+        free_info << e.text #keys
       end
       doc.css("h2#FreeAdmissionOpportunities ~ p").to_a.each do |e|
-        free_desc << e.text
+        free_desc << e.text #values
       end
-      #headings are first and then
 
+      passport_desc = free_desc.slice!(-1).split(".")
 
-
+      passport_desc.slice!(-1)
+      free_desc.slice!(-1) #useless element
+      passport_head = free_info.slice!(-1)
+      free_hash = Hash[free_info.zip(free_desc)] ########################
+      free_hash.each do |k,v|
+        puts "#{k}"
+        puts "#{v}"
+        puts ""
+      end
+      puts "#{passport_head}"
+      puts "#{passport_desc.join(".")}.)"
       #scrapes code for free admission opportunities and re-organizes it
     end #case end
   end #prices
-
-
-#  def self.scrape_exhibits(url) #creates Exhibit objects from either upcoming or current website
-#    new_exhibit = Aic::Exhibit.new
-#    doc = Nokogiri::HTML(open("#{url}"))
-#    exhibit_array = doc.css("div.view.view-exhibitions div.views-row") #creates an array of nodes to iterate over and select info
-#    y = exhibit_array.each do |xml_element|
-#      new_exhibit.title = xml_element.css("div.views-field.views-field-title span.field-content").text.tr("\n", "")
-#      new_exhibit.date_range = xml_element.css("strong.views-field.views-field-field-event-date div.field-content").text
-#      new_exhibit.location = xml_element.css("div.views-field.views-field-field-exhibition-room div.field-content").text
-#      new_exhibit.description = xml_element.css("div.views-field.views-field-body span.field-content").text
-#      new_exhibit.url = xml_element.css("div.views-field.views-field-title span.field-content a").attribute("href").text
-#      @@exhibits << new_exhibit
-#    end
-#  end #scrape_exhibits end
 
   #scraper should also scrape for EventTypes and they should be added to EventType.all if they are new and
   #scraper should scrape code from Exhibit page, Calendar page, Admission page, Description pages for individual Events and exhibits
