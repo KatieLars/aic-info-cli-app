10b55fc4ad53c632f4bb11670b1bd8eb5b999815
github username <github email address>
2017-09-11 15:48:23 -0500

    "Deleted requirement for EventDate"

--- lib/aic.rb
+++ lib/aic.rb
@@ -2,7 +2,6 @@
 require "./lib/aic/version"
 require "./lib/aic/cli"
 require "./lib/aic/info"
-require "./lib/aic/event_date"
 require "./lib/aic/event_type"
 require "./lib/aic/events"
 require "./lib/aic/exhibit"
